fe90050060d02fe12ec3426ec16c2ecdfa110217
biu <hezhou920@gmail.com>
2017-11-20 18:56:59 +0800

    Load image
    (cherry picked from commit 110f89c)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './index.css';
 import './index.less'
+import cat from './cat.jpg';
 
 function component() {
   var parent = document.createElement('div');
@@ -14,7 +15,12 @@ function component() {
 
   var des = document.createElement('p');
   des.innerHTML = 'Load less';
+  des.classList.add('highlight');
   parent.appendChild(des)
+
+  var img = new Image();
+  img.src = cat;
+  parent.appendChild(img);
 
   return parent;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
     }, {
       test: /\.less$/,
       use: ['style-loader', 'css-loader', 'less-loader']
+    }, {
+      test: /\.(jpg|png|gif)$/,
+      use: ['file-loader']
     }]
   }
 };
