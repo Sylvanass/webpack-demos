3ddfcd5ccdebce7ffaee6d9427b6749c55433592
biu <hezhou920@gmail.com>
2017-11-20 18:15:17 +0800

    Load css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import './index.css';
 
 function component() {
   var el = document.createElement('div');
   el.innerHTML = _.join([
     "Hello", 'webpack!', 'Build with configuration file.'
   ], ' ')
+  el.classList.add('hello');
 
   return el;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   }
 };
