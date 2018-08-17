4f81b5e615ecfc3960aeb59c043b96832c0ce250
biu <hezhou920@gmail.com>
2018-08-17 16:32:34 +0800

    Code Splitting: Dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import _ from 'lodash';
-
-function component() {
+async function getComponent() {
   const element = document.createElement('div');
+  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
 }
 
-document.body.appendChild(component());
+getComponent()
+  .then(component => {
+    document.body.appendChild(component);
+  });
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js',
   },
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Code splitting',
+    }),
+  ],
 };
