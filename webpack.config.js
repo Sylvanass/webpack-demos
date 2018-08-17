539f3eb2969b0a71b6905ddb348abcc5b4886fbe
biu <hezhou920@gmail.com>
2018-08-17 15:44:05 +0800

    Hot Module Replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   output: {
@@ -15,11 +15,21 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Hot Module Replacement',
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
 };
