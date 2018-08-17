c342078da66227bf6f14992a41e29f597ce632b1
biu <hezhou920@gmail.com>
2018-08-17 14:54:33 +0800

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import Background from './cool-background.png';
+import Data from './data.xml';
 
 function component() {
-  var element = document.createElement('div');
+  const element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+  const image = new Image();
+  image.src = Background;
+  element.appendChild(image);
+
+  console.log(Data);
 
   return element;
 }
